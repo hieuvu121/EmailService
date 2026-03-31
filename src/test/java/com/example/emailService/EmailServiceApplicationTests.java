d73a6d39fb82a218a0422fe8f05900fb4f416095
hieuvu121 <hieubeobilabi@gmail.com>
2026-03-31 12:26:28 +1100

    add ci properties

--- src/test/java/com/example/emailService/EmailServiceApplicationTests.java
+++ src/test/java/com/example/emailService/EmailServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.emailService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmailServiceApplicationTests {
 
 	@Test

8507572c2516a542424832c0cca408ebd320dac6
hieuvu121 <hieubeobilabi@gmail.com>
2026-04-01 14:54:43 +1100

    .

--- src/main/java/com/example/emailService/EmailServiceApplication.java
+++ src/main/java/com/example/emailService/EmailServiceApplication.java
@@ -2,10 +2,17 @@ package com.example.emailService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {

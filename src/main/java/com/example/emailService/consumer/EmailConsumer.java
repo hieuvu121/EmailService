6ac23d5d6fbe823f0589b4ddfb7cb2a1f72dbb27
hieuvu121 <hieubeobilabi@gmail.com>
2026-04-12 16:01:42 +1000

    add DLT
    # Conflicts:
    #	.github/workflows/ci.yml

--- src/main/java/com/example/emailService/consumer/EmailConsumer.java
+++ src/main/java/com/example/emailService/consumer/EmailConsumer.java
@@ -3,18 +3,25 @@ package com.example.emailService.consumer;
 import com.example.emailService.event.EmailEvent;
 import com.example.emailService.service.EmailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 //listen event-> convert data-> call service
+@RetryableTopic(attempts = "4")
 public class EmailConsumer {
     private final EmailService emailService;
+    private Logger log;
 
     @KafkaListener(topics="${kafka.topic.email}",groupId="email-id")
     public void consume(EmailEvent emailEvent){
         emailService.sendEmail(emailEvent.getTo(),emailEvent.getSubject(),emailEvent.getBody());
+    }
+
+    public void listenDLT(EmailEvent emailEvent){
+        log.info("DLT received: {}",emailEvent.getTo());
     }
 }
